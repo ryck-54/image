3ed34cd5bc01c0070d5feb91f51605c0ab1889d3
ryck-54 <ryckson.matheus@estudante.ifmt.edu.br>
2026-04-23 10:43:34 -0400

    Add files via upload

---
+++ src/main/java/application/ImageServiceimpl.java
@@ -0,0 +1,23 @@
+package application;
+
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.infra.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+
+public class ImageServiceimpl {
+
+    private final ImageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image){
+        return repository.save(image);
+    }
+
+
+}
--- src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
+++ src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
@@ -3,5 +3,5 @@ package io.spring.image.demo.infra.repository;
 import io.spring.image.demo.domain.entity.Image;
 
 public interface ImageRepository {
-    void save(Image image);
+    Image save(Image image);
 }
